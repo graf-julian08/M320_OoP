31814c9c214e663ab31b1ac773d705dd97888195
Julian Graf <graf_julian@icloud.com>
2025-08-18 10:34:32 +0200

    Add project files.

---
+++ Helloworld001/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 6;
+            int b = 7;
+            int summe = Mymath.Add(a, b);
+
+            Console.WriteLine($"Die Summe von {a} und {b} ist {summe}.");
+        }
+    }
+}
