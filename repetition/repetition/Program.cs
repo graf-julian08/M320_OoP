518aaa45aa09abc098b795212ad0cfc549ba4760
Julian Graf <graf_julian@icloud.com>
2025-08-25 10:57:34 +0200

    Repetition Aufgabe

---
+++ repetition/repetition/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace repetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            array.ArrayCalc();
+        }
+    }
+}
---
+++ repetition/repetition/array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repetition
+{
+    using System;
+
+    public class array
+    {
+        static public void ArrayCalc()
+        {
+            int[] ar = { 200, 450, 700, 175, 250 };
+
+            int summe = 0;
+            foreach (int wert in ar)
+            {
+                summe += wert;
+            }
+            Console.WriteLine("Summe: " + summe);
+        }
+    }
+}
